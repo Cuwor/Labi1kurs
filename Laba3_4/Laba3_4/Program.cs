11b6c821ba9bab1c3e69b080a162cb5daa0911cf
Cuwor <temichit@yandex.ru>
2017-11-16 13:06:06 +0700

    Сортировка

--- Laba3_4/Laba3_4/Program.cs
+++ Laba3_4/Laba3_4/Program.cs
@@ -13,36 +13,60 @@ namespace Laba3_4
             while (true)
             {
                 int n = Convert.ToInt32(Console.ReadLine());
-                int[,] array = new int[n,n];
+                int[,] array = new int[n, n];
                 for (int i = 0; i < n; i++)
                 {
                     for (int j = 0; j < n; j++)
                     {
+                        Console.WriteLine("Введи a[{0},{1}]", i, j);
                         array[i, j] = Convert.ToInt32(Console.ReadLine());
                     }
                 }
+                Show(array,n);
+                Uporyadochivanie(array, n);
             }
         }
 
-        double SredneeArifm(int[,] arr, int n)
+        static void Uporyadochivanie(int[,] a, int n)
         {
-            int sredn = 0;
-            for (int i = n-1; i > 0; i--)
-            {
-                for (int j = n - 1; j > 0; j--)
-                {
-                    if (j == i)
-                    {
-                        sredn += arr[i, j];
-                    }
-                }
-            }
-            return sredn / n;
-        }
+            //for (int i = 0; i < n; i++)
+            //    for (int j = 0; j < n; j++)
+            //        for (int g = 0; g < n - 1; g++)
+            //            if (a[j, g] < a[j, g + 1])
+            //            {
+            //                int temp = a[j, g];
+            //                a[j, g] = a[j, g + 1];
+            //                a[j, g + 1] = temp;
 
+            //            }
+            for (int i = 0; i < n; i++)
+                for (int j = 0; j < n - 1; j++)
+                    for (int g = 0; g < n; g++)
+                        if (a[j, g] < a[j + 1, g])
+                        {
+                            int temp = a[j, g];
+                            a[j, g] = a[j + 1, g];
+                            a[j + 1, g] = temp;
+                        }
+
+
+            Show(a, n);
+        }
         void ZamenaNaGlavnoy()
+            {
+
+            }
+
+        static void Show(int[,] a, int n)
         {
-            
+            for (int i = 0; i < n; i++)
+            {
+                for (int j = 0; j < n; j++)
+                {
+                    Console.Write(a[i,j] + " ");
+                }
+                Console.WriteLine();
+            }
         }
     }
 }
